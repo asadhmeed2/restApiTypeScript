d0387d1dca958c3f0c32b5868a9c0f3255948566
newasad <asadhmeed2@gmail.com>
2022-05-14 23:43:44 +0300

    add validation for user model

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,4 +1,7 @@
 import express from 'express';
+import { createUserHandler } from '../controller/user.controller';
+import { validateResource } from '../middleware/validateResourse';
+import { createUserSchema } from '../schema/user.schema';
 
 const router = express.Router();
 
@@ -6,8 +9,6 @@ router.get('/user',(_,res)=> res.status(200).json({
     message: "user is working"
 }));
 
-router.post('/api/users', (req, res)=> res.status(200).json({
-    data:req.body
-}))
+router.post('/api/users',validateResource(createUserSchema), createUserHandler)
 
 export default router;
